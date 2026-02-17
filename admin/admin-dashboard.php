8b9971325d1c6718d5d9c4116ed6c3cf1abcff91
ClintDhan <dhaniellerono@gmail.com>
2026-02-17 22:22:15 +0800

    started wireframes for admin-side // still incomplete

---
+++ admin/admin-dashboard.php
@@ -0,0 +1,38 @@
+<?php
+session_start();
+require_once '../db.php';
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Admin Dashboard</title>
+    <link rel="stylesheet" href="../asset/bootstrap-5.3.8-dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="../asset/style/style.css">
+</head>
+
+<body class="admin-body">
+    <div class="admin-dash-container">
+        <div class="admin-navbar">
+
+        </div>
+
+        <div class="admin-card-container">
+            <div class=""></div>
+            <div class=""></div>
+            <div class=""></div>
+            <div class=""></div>
+            <div class=""></div>
+        </div>
+        <div class="admin-middle-container">
+            <div class="first">first</div>
+            <div></div>
+            <div></div>
+        </div>
+    </div>
+
+    
+</body>
+</html>
