f01c841539973bb8c9f6627f690ad1e90ddcbf90
ClintDhan <dhaniellerono@gmail.com>
2026-03-28 21:36:52 +0800

    updated qr-handler.php for unused qr

--- qr-handler.php
+++ qr-handler.php
@@ -7,16 +7,24 @@ if (!isset($_GET['control'])) {
 }
 
 $control = $_GET['control'];
+$sql = "SELECT * FROM qr_code WHERE control_num = '$control'";
+$result = $conn->query($sql);
+$row = $result->fetch_assoc();
 
 // Absolute base path for your project
 $base = '/DocumentTrackingSys';
 
 // Logged-in users → redirect based on role
 if (isset($_SESSION['user_id'])) {
-    if ($_SESSION['role'] == 'admin' || $_SESSION['role'] == 'superadmin') {
+    if (($_SESSION['role'] == 'admin' || $_SESSION['role'] == 'superadmin') && $row['is_used'] == 1) {
         header("Location: $base/admin/admin-view-document.php?control=" . urlencode($control));
         exit;
-    } else {
+    }
+    elseif(($_SESSION['role'] == 'admin' || $_SESSION['role'] == 'superadmin') && $row['is_used'] == 0) {
+        header("Location: $base/admin/admin-receive-document.php?control=" . urlencode($control));
+        exit;
+    }
+    else {
         header("Location: $base/user/user-home.php?control=" . urlencode($control));
         exit;
     }
